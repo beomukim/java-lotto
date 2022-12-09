92a2fb055c49b19ec53056f8e2e8a03dbc23f42e
bumwoo <gld55602@gmail.com>
2022-12-09 21:18:00 +0900

    feat:

--- src/main/java/lotto/domain/LottoMaker.java
+++ src/main/java/lotto/domain/LottoMaker.java
@@ -1,8 +1,15 @@
 package lotto.domain;
 
+import static lotto.util.Rules.LOTTO_COUNT;
+import static lotto.util.Rules.WINNING_NUMBER_END_NUMBER;
+import static lotto.util.Rules.WINNING_NUMBER_START_NUMBER;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import lotto.util.Rules;
 
 public class LottoMaker {
     private int count;
@@ -15,8 +22,9 @@ public class LottoMaker {
         List<Lotto> lottos = new ArrayList<>();
 
         for (int i = 0; i < count; i++) {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            lottos.add(new Lotto(numbers));
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(WINNING_NUMBER_START_NUMBER,
+                    WINNING_NUMBER_END_NUMBER, LOTTO_COUNT);
+            lottos.add(new Lotto(numbers.stream().sorted().collect(Collectors.toList())));
         }
 
         return lottos;
